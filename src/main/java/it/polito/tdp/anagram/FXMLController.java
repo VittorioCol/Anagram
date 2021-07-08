1f21d069599ce519b16ee3a57c1445f860d9e868
vitto <vitto@192.168.1.24>
2021-07-08 13:00:05 +0200

    finale

--- src/main/java/it/polito/tdp/anagram/FXMLController.java
+++ src/main/java/it/polito/tdp/anagram/FXMLController.java
@@ -1,25 +1,51 @@
 package it.polito.tdp.anagram;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.anagram.model.Anagram;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField parola;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private TextArea anagrammi;
+
+    @FXML
+    void doAnagram(ActionEvent event) {
+    	anagrammi.clear();
+    	String p=parola.getText();
+    	Anagram a=new Anagram();
+    	List<String> risultato=a.anagrammi(p);
+    	for(String r:risultato) {
+    		anagrammi.appendText(r+"\n");
+    	}
+    	anagrammi.appendText(""+risultato.size());
+    	parola.clear();
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void initialize() {
+        assert parola != null : "fx:id=\"parola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert button != null : "fx:id=\"button\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert anagrammi != null : "fx:id=\"anagrammi\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
--- src/main/java/it/polito/tdp/anagram/model/Anagram.java
+++ src/main/java/it/polito/tdp/anagram/model/Anagram.java
@@ -8,43 +8,22 @@ public class Anagram {
 	public List<String> anagrammi(String parola) {
 		
 		List<String> risultato = new ArrayList<>() ;
-		permuta("", parola, 0, risultato) ; // LANCIA la ricorsione
-		// cancella dalla lista le parole non valide (leggendo il dizionario)
+		permuta("", parola, 0, risultato) ;
 		return risultato ;
 		
 	}
-	
-	// livello = lunghezza della soluzione parziale
-	// livello iniziale = 0
-	// parziale = stringa che contiene l'anagramma incompleto in fase di costruzione
-	// lettere = le lettere della parola iniziale che ancora non abbiamo utilizzato
-	//           (=== il sotto-problema che dobbiamo risolvere)
+
 	
 	private void permuta(String parziale, String lettere, int livello, List<String> risultato) {
-		if( lettere.length()==0 ) { // caso terminale 
-			// la soluzione parziale è anche una soluzione completa!!
-
-			// if(parziale è una parola valida?) {
+		if( lettere.length()==0 ) {
 			risultato.add(parziale) ;
 		} else {
-			// fai ricorsione
-			// sottoproblema == una lettera (un singolo carattere) di 'lettere'
+			
 			for(int pos=0; pos<lettere.length(); pos++) {
-				
 				char tentativo = lettere.charAt(pos) ; 
-				
 				String nuovaParziale = parziale + tentativo ;
 				String nuovaLettere = lettere.substring(0, pos)+lettere.substring(pos+1) ;
-					// togli il carattere pos da lettere
-				
-				// if (nuovaParziale è un PREFISSO valido di almeno una parola nel dizionario)
-				// "aqz" -> no ; "car" -> si (carro, carrello, carta...)
-				permuta(nuovaParziale, nuovaLettere, livello+1, risultato) ;
-				
-				// Backtracking (NON SERVE)
-				// rimetti a posto 'parziale'
-				// rimetti a posto 'lettere'
-				
+				permuta(nuovaParziale, nuovaLettere, livello+1, risultato) ;	
 			}
 		}
 	}
